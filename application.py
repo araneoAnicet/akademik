1957ca4acfee685aec22a7bc6bcfff83dd279d27
araneoAnicet <root@localhost.localdomain>
2019-08-04 23:42:11 +0100

    fix database relations

--- application.py
+++ application.py
@@ -3,5 +3,6 @@ from flask import Flask
 app = Flask(__name__)
 
 
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
 app.config['SECRET_KEY'] = 'APP_SECRET_KEY'
--- db_management.py
+++ db_management.py
@@ -3,12 +3,29 @@ from flask_sqlalchemy import SQLAlchemy
 
 db = SQLAlchemy(app)
 
+books = db.Table('books',
+    db.Column('user_id', db.Integer, db.ForeignKey('user.id'), primary_key=True),
+    db.Column('day_id', db.Integer, db.ForeignKey('day.id'), primary_key=True)
+)
+
+
+accepted = db.Table('accepted',
+    db.Column('user_id', db.Integer, db.ForeignKey('user.id'), primary_key=True),
+    db.Column('day_id', db.Integer, db.ForeignKey('day.id'), primary_key=True)
+)
+
+
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(40), nullable=False)
     surname = db.Column(db.String(60), nullable=False)
-    email = db.Column(db.String(80), unique=True, nullable=True)
+    email = db.Column(db.String(80), unique=True, nullable=False)
     password = db.Column(db.String(40), nullable=False)
+
+    requested_days = db.relationship('Day', secondary=books, lazy='subquery',
+        backref=db.backref('requested_by', lazy=True))
+
+    accepted_days = db.relationship('Day', secondary=accepted, lazy='subquery', backref=db.backref('accepted_candidates', lazy=True))
 
     def _repr_(self):
         return f'{self.name} {self.surname}'
@@ -18,8 +35,6 @@ class Day(db.Model):
     day = db.Column(db.Integer, nullable=False)
     month = db.Column(db.Integer, nullable=False)
     year = db.Column(db.Integer, nullable=False)
-    candidates = db.relationship('User', backref='requested_by', lazy='joined')
-    booked_by = db.relationship('User', backref='bookers', lazy='joined')
 
     def _repr_(self):
         return f'{self.day}.{self.month}.{self.year}'
