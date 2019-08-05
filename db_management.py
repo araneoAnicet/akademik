88d95daa42cf4347651d2817539e99ebba69696e
araneoAnicet <araneoanicet@gmail.com>
2019-08-05 01:47:27 +0100

    add password encryption functions

--- db_management.py
+++ db_management.py
@@ -1,5 +1,6 @@
 from application import app
 from flask_sqlalchemy import SQLAlchemy
+from passlib.hash import sha256_crypt
 
 db = SQLAlchemy(app)
 
@@ -20,21 +21,33 @@ class User(db.Model):
     name = db.Column(db.String(40), nullable=False)
     surname = db.Column(db.String(60), nullable=False)
     email = db.Column(db.String(80), unique=True, nullable=False)
-    password = db.Column(db.String(40), nullable=False)
+    password = db.Column(db.String(100), nullable=False)
 
     requested_days = db.relationship('Day', secondary=books, lazy='subquery',
         backref=db.backref('requested_by', lazy=True))
 
     accepted_days = db.relationship('Day', secondary=accepted, lazy='subquery', backref=db.backref('accepted_candidates', lazy=True))
 
-    def _repr_(self):
+    def __repr__(self):
         return f'{self.name} {self.surname}'
 
 class Day(db.Model):
     id = db.Column(db.Integer, primary_key=True)
+    week_day = db.Column(db.String(3), nullable=False)
     day = db.Column(db.Integer, nullable=False)
     month = db.Column(db.Integer, nullable=False)
     year = db.Column(db.Integer, nullable=False)
 
-    def _repr_(self):
+    def __repr__(self):
         return f'{self.day}.{self.month}.{self.year}'
+
+
+def user_check(email):
+    searched_user = User.query.filter_by(email=email).first()
+    return True if searched_user else False
+
+def password_encrypt(password):
+    return sha256_crypt.encrypt(password)
+
+def password_verify(user, password):
+    return sha256_crypt.verify(password, user.password)
