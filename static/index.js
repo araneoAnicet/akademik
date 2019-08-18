da83445edd560292e3b0dd4a26c324c31c736775
araneoAnicet <araneoanicet@gmail.com>
2019-08-18 23:01:48 +0100

    add admin page

--- static/index.js
+++ static/index.js
@@ -1,8 +1,8 @@
 import Vue from 'https://cdn.jsdelivr.net/npm/vue@2.6.10/dist/vue.esm.browser.js'
 
-new Vue({
-    el: '#ui',
+var app = new Vue({
+    el: '#app',
     data: {
-        show: false
+        show: false,
     }
 })
