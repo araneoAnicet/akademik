c8d0432aa85dc88f277c453236edcc694e0e5b58
Nikita Solomientsev <araneoanicet@gmail.com>
2019-08-29 04:20:11 +0300

    add basic API routes

--- flaskapp/api/routes.py
+++ flaskapp/api/routes.py
@@ -1,5 +1,8 @@
 from flask import Blueprint, request, Response, jsonify
-from flaskapp.models import db, User, Day, Profilechange
+from flaskapp import db
+from flaskapp.models import DatabaseManager, User, Day, Profilechange
+
+dm = DatabaseManager(db, User, Day, Profilechange)
 
 mod = Blueprint('api', __name__, url_prefix='/api')
 
@@ -20,14 +23,86 @@ def response_format(
     }), status
 
 
-@mod.route('/get_users', methods=['GET'])
+@mod.route('/get_unregistered_users', methods=['GET'])
 def get_users():
     return response_format(data={
-        'users': [
-            {
+        'users': list(map(
+            lambda user: {
                 'name': user.name,
                 'surname': user.surname,
-                'room': user.room
-            }
-        ] for user in User.query.all()
+                'room': user.room,
+                'email': user.email
+            }, dm.get_unregistered_users()
+        ))
     })
+
+@mod.route('/accept_user_registration/<email>', methods=['PUT'])
+def accept_registration(email):
+    searched_user = dm.get_user(email)
+    searched_user.is_registered = True
+    db.session.commit()
+    return response_format(
+        message='The user has been accepted',
+        data={
+            'user': {
+                'name': searched_user.name,
+                'surname': searched_user.surname,
+                'room': searched_user.room,
+                'email': searched_user.email
+            }
+        }
+    )
+
+@mod.route('/get_profilechanges', methods=['GET'])
+def get_profilechanges():
+    return response_format(
+        data={
+            list(map(lambda change: 
+                {'change': {
+                    'user': {
+                        'name': change.requested_by[0].name,
+                        'surname': change.requested_by[0].surname,
+                        'room': change.requested_by[0].room,
+                        'email': change.requested_by[0].email
+                    },
+                    'to_commit': {
+                        'name': change.name,
+                        'surname': change.surname,
+                        'room': change.room,
+                    }
+                }}, dm.get_profilechanges()
+            ))
+            
+        }
+    )
+
+@mod.route('/accept_profilechange/<email>')
+def accept_profilechange(email):
+    searched_user = dm.get_user(email)
+    user_copy = searched_user.copy()
+    change = searched_user.requested_changes.first()
+    change_copy = change.copy()
+
+    searched_user.name = change.name
+    searched_user.surname = change.surname
+    searched_user.room = change.room
+
+    db.session.remove(change)
+    db.session.commit()
+    return response_format(
+        message='Changes have been commited',
+        data={
+            'user_before': {
+                'name': user_copy.name,
+                'surname': user_copy.surname,
+                'room': user_copy.room,
+                'email': user_copy.email
+            },
+            'changes': {
+                'name': change_copy.name,
+                'surname': change_copy.surname,
+                'room': change_copy.room,
+                'email': searched_user.email
+            }
+        }
+    )
